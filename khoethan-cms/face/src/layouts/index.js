1ccdd4e4eaa0af7ed9a1e40e1e3f90ed6f290681
HtetHtetWin23 <134780909+HtetHtetWin23@users.noreply.github.com>
2023-05-27 21:00:05 +0630

    Add files via upload

---
+++ khoethan-cms/face/src/layouts/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Layout } from 'antd';
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
+import { renderRoutes } from 'react-router-config'
+import PageLoading from '../components/PageLoading';
+
+
+const { Content } = Layout;
+class Root extends React.Component {
+    render() {
+        const { route:{routes} } = this.props
+        return (
+            <Layout>
+                <Navbar />
+                <Content>
+                {
+                  routes ? renderRoutes(routes)
+                  :
+                  <PageLoading />
+                }
+                
+              </Content>
+                <Footer />
+            </Layout>            
+        );
+    }
+}
+
+export default Root;
